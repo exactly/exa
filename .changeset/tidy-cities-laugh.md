8fae5245d4ac9ebb881a376575d13773f37630da
danilo neves cruz <cruzdanilo@gmail.com>
2025-04-18 12:13:31 -0300

    🥅 server: recover from missing sent transaction
    co-authored-by: nfmelendez <nfmelendez@gmail.com>

---
+++ .changeset/tidy-cities-laugh.md
@@ -0,0 +1,5 @@
+---
+"@exactly/server": patch
+---
+
+🥅 recover from missing sent transaction
---
+++ server/test/utils/keeper.test.ts
@@ -0,0 +1,54 @@
+import "../mocks/sentry";
+import "../mocks/deployments";
+import "../mocks/keeper";
+
+import type * as timers from "node:timers/promises";
+import { afterEach, describe, expect, inject, it, vi } from "vitest";
+
+import { auditorAbi } from "../../generated/contracts";
+import keeper from "../../utils/keeper";
+import publicClient from "../../utils/publicClient";
+
+describe("fault tolerance", () => {
+  it("recovers if transaction is missing", async () => {
+    const sendRawTransaction = vi.spyOn(publicClient, "sendRawTransaction");
+    sendRawTransaction.mockResolvedValueOnce("0x");
+    const onHash = vi.fn<() => void>();
+    const receipt = await keeper.exaSend(
+      { name: "test transfer", op: "test.transfer" },
+      { address: inject("Auditor"), abi: auditorAbi, functionName: "enterMarket", args: [inject("MarketUSDC")] },
+      { onHash },
+    );
+
+    expect(onHash).toHaveBeenCalledOnce();
+    expect(receipt?.status).toBe("success");
+    expect(sendRawTransaction).toHaveBeenCalledTimes(2);
+  });
+
+  it("times out if can't send transaction", async () => {
+    const waitForTransactionReceipt = publicClient.waitForTransactionReceipt;
+    vi.spyOn(publicClient, "waitForTransactionReceipt").mockImplementation((parameters) =>
+      waitForTransactionReceipt({ ...parameters, timeout: 1100 }),
+    );
+    const sendRawTransaction = vi.spyOn(publicClient, "sendRawTransaction");
+    sendRawTransaction.mockResolvedValue("0x");
+    const onHash = vi.fn<() => void>();
+
+    await expect(
+      keeper.exaSend(
+        { name: "test transfer", op: "test.transfer" },
+        { address: inject("Auditor"), abi: auditorAbi, functionName: "enterMarket", args: [inject("MarketUSDC")] },
+        { onHash },
+      ),
+    ).rejects.toThrow("Timed out while waiting for transaction");
+    expect(onHash).toHaveBeenCalledOnce();
+    expect(sendRawTransaction).toHaveBeenCalledTimes(3);
+  });
+});
+
+vi.mock("node:timers/promises", async (importOriginal) => {
+  const original = await importOriginal<typeof timers>();
+  return { ...original, setTimeout: (...arguments_: unknown[]) => original.setTimeout(500, ...arguments_.slice(1)) };
+});
+
+afterEach(() => vi.resetAllMocks());
