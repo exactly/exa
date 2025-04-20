8d28478e8813a4d939d07d601e8e5c5fe0c1be2c
danilo neves cruz <cruzdanilo@gmail.com>
2025-04-20 19:48:01 -0300

    📈 server: instrument ens http calls

--- server/utils/ensClient.ts
+++ server/utils/ensClient.ts
@@ -1,8 +1,16 @@
 import { mainnet } from "@alchemy/aa-core";
 import alchemyAPIKey from "@exactly/common/alchemyAPIKey";
+import { parse } from "valibot";
 import { createPublicClient, http } from "viem";
+
+import { captureRequests, Requests } from "./publicClient";
 
 export default createPublicClient({
   chain: mainnet,
-  transport: http(`${mainnet.rpcUrls.alchemy?.http[0]}/${alchemyAPIKey}`, { batch: true }),
+  transport: http(`${mainnet.rpcUrls.alchemy?.http[0]}/${alchemyAPIKey}`, {
+    batch: true,
+    async onFetchRequest(request) {
+      captureRequests(parse(Requests, await request.json()));
+    },
+  }),
 });
