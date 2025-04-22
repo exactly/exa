6fc579a1252d046deb1de9f749d9e998eb87ce07
danilo neves cruz <cruzdanilo@gmail.com>
2025-04-22 20:40:28 -0300

    ⚡️ server: batch keeper http requests

--- server/utils/keeper.ts
+++ server/utils/keeper.ts
@@ -29,7 +29,9 @@ if (!chain.rpcUrls.alchemy?.http[0]) throw new Error("missing alchemy rpc url");
 
 export default createWalletClient({
   chain,
-  transport: http(`${chain.rpcUrls.alchemy.http[0]}/${alchemyAPIKey}`),
+  transport: http(`${chain.rpcUrls.alchemy.http[0]}/${alchemyAPIKey}`, {
+    batch: true,
+  }),
   account: privateKeyToAccount(
     parse(Hash, process.env.KEEPER_PRIVATE_KEY, {
       message: "invalid keeper private key",
