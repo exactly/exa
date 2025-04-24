0b996b1899fbda582b07e25fdad9bdeced2bf30b
franm <fran.marienhoff@gmail.com>
2025-04-24 15:59:46 -0300

    🔥 app: remove unused components

--- src/app/(app)/simulate-purchase/_layout.tsx
+++
@@ -1,9 +0,0 @@
-import { Stack } from "expo-router";
-import React from "react";
-
-import useBackgroundColor from "../../../utils/useBackgroundColor";
-
-export default function SimulatePurchaseLayout() {
-  useBackgroundColor();
-  return <Stack initialRouteName="index" screenOptions={{ headerShown: false }} />;
-}
