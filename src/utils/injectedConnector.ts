17c597bdd90308efedf1f2c1b48ef8327be06d70
danilo neves cruz <cruzdanilo@gmail.com>
2025-05-14 20:50:01 -0300

    🚧 app: implement sign-in with ethereum

---
+++ src/utils/injectedConnector.ts
@@ -0,0 +1,46 @@
+import chain from "@exactly/common/generated/chain";
+import type { Passkey } from "@exactly/common/validation";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { isAddress, type Address } from "viem";
+import { createConfig, createStorage, custom, injected } from "wagmi";
+
+import publicClient from "./publicClient";
+import queryClient from "./queryClient";
+import reportError from "./reportError";
+
+export const config = createConfig({
+  chains: [chain],
+  connectors: [injected()],
+  transports: { [chain.id]: custom(publicClient) },
+  storage: createStorage({ storage: AsyncStorage }),
+});
+
+if (config.connectors[0]?.id !== "injected") throw new Error("no injected connector");
+export const [connector] = config.connectors;
+
+export async function connectAccount(account: Address) {
+  const accounts = await connector.isAuthorized().then(async (isAuthorized) => {
+    if (isAuthorized) return connector.getAccounts();
+    if (!(await connector.getProvider({ chainId: chain.id }))) throw new Error("no injected provider");
+    const { accounts: connectedAccounts } = await connector.connect({ chainId: chain.id });
+    return connectedAccounts;
+  });
+  if (!accounts.includes(account)) throw new Error("injected account mismatch");
+  return account;
+}
+
+export async function getAccount() {
+  try {
+    const passkey = queryClient.getQueryData<Passkey>(["passkey"]);
+    if (passkey) return isAddress(passkey.credentialId) ? passkey.credentialId : undefined;
+    if (await connector.isAuthorized()) {
+      const accounts = await connector.getAccounts();
+      return accounts[0];
+    }
+    if (!(await connector.getProvider({ chainId: chain.id }))) return;
+    const { accounts } = await connector.connect({ chainId: chain.id });
+    return accounts[0];
+  } catch (error: unknown) {
+    reportError(error);
+  }
+}
