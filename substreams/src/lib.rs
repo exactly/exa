611257efd65b6018a43d9a8348352149c23fed2d
itofarina <farinalvaro@gmail.com>
2025-05-30 13:59:45 +0200

    🚧 debug - event fix

--- substreams/src/lib.rs
+++ substreams/src/lib.rs
@@ -1,5 +1,5 @@
 use abi::{
-  entrypoint::events::AccountDeployed, erc20::events::Transfer, lifi::events::LiFiSwappedGeneric,
+  entrypoint::events::AccountDeployed, erc20::events::Transfer, lifi::events::LiFiGenericSwapCompleted,
   market::events::Deposit,
 };
 use proto::exa;
@@ -14,17 +14,15 @@ use substreams_ethereum::{pb::eth::v2::Block, Event};
 
 mod abi;
 #[allow(clippy::all)]
-#[allow(clippy::all)]
 mod proto;
 
-const MARKET_WETH: [u8; 20] = hex!("c4d4500326981eacD020e20A81b1c479c161c7EF");
-const MARKET_USDC: [u8; 20] = hex!("6926B434CCe9b5b7966aE1BfEef6D0A7DCF3A8bb");
-const MARKET_USDCE: [u8; 20] = hex!("7F5c764cBc14f9669B88837ca1490cCa17c31607");
-const MARKET_WSTETH: [u8; 20] = hex!("22ab31Cd55130435b5efBf9224b6a9d5EC36533F");
-const MARKET_WBTC: [u8; 20] = hex!("6f748FD65d7c71949BA6641B3248C4C191F3b322");
-const MARKET_OP: [u8; 20] = hex!("a430A427bd00210506589906a71B54d6C256CEdb");
-
-const MARKETS: [[u8; 20]; 6] = [MARKET_WETH, MARKET_USDC, MARKET_USDCE, MARKET_WSTETH, MARKET_WBTC, MARKET_OP];
+// const MARKET_WETH: [u8; 20] = hex!("c4d4500326981eacD020e20A81b1c479c161c7EF");
+// const MARKET_USDC: [u8; 20] = hex!("6926B434CCe9b5b7966aE1BfEef6D0A7DCF3A8bb");
+// const MARKET_USDCE: [u8; 20] = hex!("7F5c764cBc14f9669B88837ca1490cCa17c31607");
+// const MARKET_WSTETH: [u8; 20] = hex!("22ab31Cd55130435b5efBf9224b6a9d5EC36533F");
+// const MARKET_WBTC: [u8; 20] = hex!("6f748FD65d7c71949BA6641B3248C4C191F3b322");
+// const MARKET_OP: [u8; 20] = hex!("a430A427bd00210506589906a71B54d6C256CEdb");
+// const MARKETS: [[u8; 20]; 6] = [MARKET_WETH, MARKET_USDC, MARKET_USDCE, MARKET_WSTETH, MARKET_WBTC, MARKET_OP];
 
 #[substreams::handlers::map]
 pub fn map_accounts(block: Block) -> Result<exa::Accounts, Error> {
@@ -91,23 +89,24 @@ pub fn db_deposits(
 
 #[substreams::handlers::map]
 pub fn map_deposits(block: Block) -> Result<exa::Deposits, Error> {
+  // const MARKET_WETH: [u8; 20] = hex!("c4d4500326981eacD020e20A81b1c479c161c7EF");
   Ok(exa::Deposits {
     deposits: block
       .logs()
       .filter_map(|log| {
         Deposit::match_and_decode(log).and_then(|event| {
-          if MARKETS.contains(&log.address().to_vec().try_into().unwrap()) {
-            Some(exa::Deposit {
-              market: log.address().to_vec(),
-              receiver: event.owner.to_vec(),
-              amount: event.assets.to_string(),
-              block_number: block.number,
-              tx_index: log.receipt.transaction.index as u64,
-              log_index: log.index() as u64,
-            })
-          } else {
-            None
-          }
+          // if log.address().to_vec() == MARKET_WETH {
+          Some(exa::Deposit {
+            market: log.address().to_vec(),
+            receiver: event.owner.to_vec(),
+            amount: event.assets.to_string(),
+            block_number: block.number,
+            tx_index: log.receipt.transaction.index as u64,
+            log_index: log.index() as u64,
+          })
+          // } else {
+          //   None
+          // }
         })
       })
       .collect(),
@@ -120,9 +119,9 @@ pub fn map_swaps(block: Block) -> Result<exa::Swaps, Error> {
     swaps: block
       .logs()
       .filter_map(|log| {
-        LiFiSwappedGeneric::match_and_decode(log).and_then(|event| {
+        LiFiGenericSwapCompleted::match_and_decode(log).and_then(|event| {
           Some(exa::Swap {
-            receiver: Default::default(),
+            receiver: event.receiver.to_vec(),
             asset_from: event.from_asset_id.to_vec(),
             asset_to: event.to_asset_id.to_vec(),
             amount_from: event.from_amount.to_string(),
