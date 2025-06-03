527eaceafe951f82912019fdb779a7402736be3c
danilo neves cruz <cruzdanilo@gmail.com>
2025-06-03 22:00:15 +0100

    🎉 substreams: create substreams project

---
+++ substreams/build.rs
@@ -0,0 +1,64 @@
+use anyhow::{Error, Ok, Result};
+use std::{
+  env::var,
+  fs::{canonicalize, create_dir_all},
+  process::Command,
+};
+use substreams_ethereum::Abigen;
+
+fn main() -> Result<(), Error> {
+  println!("cargo::rerun-if-changed=proto");
+  println!("cargo::rerun-if-changed=substreams.yaml");
+  println!("cargo::rerun-if-changed=../contracts/script/ExaAccountFactory.s.sol");
+
+  create_dir_all("abi")?;
+  let contracts = [("factory", "ExaAccountFactory")];
+
+  assert!(Command::new("bash")
+    .arg("-c")
+    .arg("forge build")
+    .current_dir("../contracts")
+    .status()
+    .expect("forge build failed")
+    .success());
+
+  contracts.iter().for_each(|(mod_name, contract_name)| {
+    assert!(Command::new("bash")
+      .arg("-c")
+      .arg(format!("jq .abi ../contracts/out/{contract_name}.sol/{contract_name}.json > abi/{mod_name}.json"))
+      .status()
+      .expect("abi extraction failed")
+      .success());
+
+    Abigen::new(*contract_name, &format!("abi/{mod_name}.json"))
+      .unwrap()
+      .generate()
+      .unwrap()
+      .write_to_file(format!("src/abi/{mod_name}.rs"))
+      .unwrap();
+  });
+
+  assert!(Command::new("substreams")
+    .arg("protogen")
+    .arg("--exclude-paths=sf/substreams,google")
+    .env("PATH", format!("{}:{}", canonicalize("node_modules/.bin")?.display(), var("PATH").unwrap_or_default()))
+    .status()
+    .expect("protogen failed")
+    .success());
+
+  assert!(Command::new("bash")
+    .arg("-c")
+    .arg(format!(
+      "echo -e '{}' > src/abi/mod.rs",
+      contracts
+        .iter()
+        .map(|(mod_name, _)| format!("#[allow(clippy::all)]\\npub mod {mod_name};"))
+        .collect::<Vec<_>>()
+        .join("\\n")
+    ))
+    .status()
+    .expect("mod.rs generation failed")
+    .success());
+
+  Ok(())
+}
