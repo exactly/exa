6962742410c05f522c31cc492dfbfd8541a4762b
franm <fran.marienhoff@gmail.com>
2025-06-18 18:57:51 -0300

    ✨ app: implement swaps

---
+++ src/components/swaps/Success.tsx
@@ -0,0 +1,139 @@
+import type { Token } from "@lifi/sdk";
+import { ArrowDown, ArrowRight } from "@tamagui/lucide-icons";
+import { LinearGradient } from "expo-linear-gradient";
+import { router } from "expo-router";
+import React from "react";
+import { Pressable, Image } from "react-native";
+import { ScrollView, Square, styled, useTheme, XStack, YStack } from "tamagui";
+import { formatUnits } from "viem";
+
+import queryClient from "../../utils/queryClient";
+import reportError from "../../utils/reportError";
+import Button from "../shared/Button";
+import SafeView from "../shared/SafeView";
+import ExaSpinner from "../shared/Spinner";
+import Text from "../shared/Text";
+import TransactionDetails from "../shared/TransactionDetails";
+import View from "../shared/View";
+
+export default function Success({
+  fromUsdAmount,
+  fromAmount,
+  fromToken,
+  toUsdAmount,
+  toAmount,
+  toToken,
+}: {
+  fromUsdAmount: number;
+  fromAmount: bigint;
+  fromToken: Token;
+  toUsdAmount: number;
+  toAmount: bigint;
+  toToken: Token;
+}) {
+  const theme = useTheme();
+  return (
+    <View fullScreen backgroundColor="$backgroundSoft">
+      <StyledGradient
+        locations={[0.5, 1]}
+        position="absolute"
+        top={0}
+        left={0}
+        right={0}
+        height={220}
+        opacity={0.2}
+        colors={[theme.uiInfoSecondary.val, theme.backgroundSoft.val]}
+      />
+      <SafeView backgroundColor="transparent">
+        <View fullScreen padded>
+          <ScrollView
+            showsVerticalScrollIndicator={false}
+            stickyHeaderIndices={[0]}
+            // eslint-disable-next-line react-native/no-inline-styles
+            contentContainerStyle={{
+              flexGrow: 1,
+              flexDirection: "column",
+              justifyContent: "space-between",
+            }}
+            stickyHeaderHiddenOnScroll
+          >
+            <View flex={1}>
+              <YStack gap="$s7" paddingBottom="$s9">
+                <XStack justifyContent="center" alignItems="center">
+                  <Square borderRadius="$r4" backgroundColor="$interactiveBaseInformationSoftDefault" size={80}>
+                    <ExaSpinner backgroundColor="transparent" color="$uiInfoSecondary" />
+                  </Square>
+                </XStack>
+                <YStack gap="$s4_5" justifyContent="center" alignItems="center">
+                  <Text secondary body>
+                    Swap request{" "}
+                    <Text secondary body emphasized>
+                      sent
+                    </Text>
+                  </Text>
+                  <Text title primary color="$uiNeutralPrimary">
+                    {fromUsdAmount.toLocaleString(undefined, {
+                      style: "currency",
+                      currency: "USD",
+                      currencyDisplay: "narrowSymbol",
+                    })}
+                  </Text>
+                  <XStack gap="$s2" alignItems="center">
+                    <Image source={{ uri: fromToken.logoURI }} width={16} height={16} borderRadius={20} />
+                    <Text emphasized secondary subHeadline>
+                      {Number(formatUnits(fromAmount, fromToken.decimals)).toFixed(8)}
+                    </Text>
+                  </XStack>
+                  <ArrowDown size={24} color="$interactiveBaseBrandDefault" />
+                  <Text title primary color="$uiNeutralPrimary">
+                    {toUsdAmount.toLocaleString(undefined, {
+                      style: "currency",
+                      currency: "USD",
+                      currencyDisplay: "narrowSymbol",
+                    })}
+                  </Text>
+                  <XStack gap="$s2" alignItems="center">
+                    <Image source={{ uri: toToken.logoURI }} width={16} height={16} borderRadius={20} />
+                    <Text emphasized secondary subHeadline>
+                      {Number(formatUnits(toAmount, toToken.decimals)).toFixed(8)}
+                    </Text>
+                  </XStack>
+                </YStack>
+              </YStack>
+              <TransactionDetails />
+            </View>
+            <View flex={2} justifyContent="flex-end">
+              <YStack alignItems="center" gap="$s4">
+                <Button
+                  onPress={() => {
+                    queryClient.invalidateQueries({ queryKey: ["swap"] }).catch(reportError);
+                    router.replace("/(app)/pending-proposals");
+                  }}
+                  contained
+                  main
+                  fullwidth
+                  spaced
+                  iconAfter={<ArrowRight size={16} />}
+                >
+                  View pending request
+                </Button>
+                <Pressable
+                  onPress={() => {
+                    queryClient.invalidateQueries({ queryKey: ["swap"] }).catch(reportError);
+                    router.replace("/(app)/(home)");
+                  }}
+                >
+                  <Text emphasized footnote color="$uiBrandSecondary">
+                    Close
+                  </Text>
+                </Pressable>
+              </YStack>
+            </View>
+          </ScrollView>
+        </View>
+      </SafeView>
+    </View>
+  );
+}
+
+const StyledGradient = styled(LinearGradient, {});
---
+++ src/components/swaps/TokenInput.tsx
@@ -0,0 +1,167 @@
+import { WAD } from "@exactly/lib";
+import type { Token } from "@lifi/sdk";
+import { useForm } from "@tanstack/react-form";
+import React, { useCallback, useEffect } from "react";
+import { Image } from "react-native";
+import { styled, XStack, YStack } from "tamagui";
+import { pipe, string, nonEmpty } from "valibot";
+import { formatUnits, parseUnits } from "viem";
+
+import OptimismImage from "../../assets/images/optimism.svg";
+import Input from "../shared/Input";
+import Skeleton from "../shared/Skeleton";
+import Text from "../shared/Text";
+import View from "../shared/View";
+
+export default function TokenInput({
+  label,
+  token,
+  amount,
+  balance,
+  isLoading = false,
+  isActive,
+  isDanger,
+  onTokenSelect,
+  onFocus,
+  onChange,
+}: {
+  label: string;
+  token?: Token;
+  amount: bigint;
+  balance: bigint;
+  isLoading?: boolean;
+  isActive: boolean;
+  isDanger?: boolean;
+  onTokenSelect: () => void;
+  onFocus?: () => void;
+  onChange?: (amount: bigint) => void;
+}) {
+  const { Field, setFieldValue, getFieldValue } = useForm({ defaultValues: { amountInput: "" } });
+
+  const handleAmountChange = useCallback(
+    (value: string) => {
+      setFieldValue("amountInput", value);
+      if (!token) return;
+      const inputAmount = parseUnits(value.replaceAll(/\D/g, ".").replaceAll(/\.(?=.*\.)/g, ""), token.decimals);
+      onChange?.(inputAmount);
+    },
+    [setFieldValue, token, onChange],
+  );
+
+  const valueUSD =
+    amount && token ? Number(formatUnits((amount * parseUnits(token.priceUSD, 18)) / WAD, token.decimals)) : 0;
+
+  useEffect(() => {
+    if (!isActive && token) {
+      setFieldValue("amountInput", amount > 0n ? formatUnits(amount, token.decimals) : getFieldValue("amountInput"));
+    }
+  }, [isActive, amount, token, setFieldValue, getFieldValue]);
+
+  useEffect(() => {
+    setFieldValue("amountInput", "");
+  }, [setFieldValue, token]);
+
+  return (
+    <YStack
+      borderWidth={1}
+      borderColor={isDanger ? "$borderErrorStrong" : isActive ? "$borderBrandStrong" : "$borderNeutralSoft"}
+      borderRadius="$r3"
+      padding="$s4_5"
+      gap="$s4_5"
+    >
+      <Text emphasized footnote color="$uiNeutralSecondary">
+        {label}
+      </Text>
+      <XStack gap="$s3_5" alignItems="center">
+        <TokenImageContainer onPress={onTokenSelect}>
+          {token ? (
+            <>
+              <Image source={{ uri: token.logoURI }} width={40} height={40} borderRadius={16} />
+              <ChainLogoWrapper>
+                <OptimismImage width="100%" height="100%" />
+              </ChainLogoWrapper>
+            </>
+          ) : (
+            <Skeleton radius="round" height={40} width={40} />
+          )}
+        </TokenImageContainer>
+        <YStack flex={1} gap="$s2">
+          <XStack alignItems="center" gap="$s2" justifyContent="space-between">
+            {isLoading && !isActive ? (
+              <View flex={1}>
+                <Skeleton height={28} width="100%" />
+              </View>
+            ) : (
+              <Field name="amountInput" validators={{ onChange: pipe(string(), nonEmpty("empty")) }}>
+                {({ state }) => (
+                  <AmountInput
+                    value={state.value}
+                    onChangeText={handleAmountChange}
+                    onFocus={onFocus}
+                    placeholder={amount.toString()}
+                    color={isDanger ? "$uiErrorSecondary" : isActive ? "$uiNeutralPrimary" : "$uiNeutralPlaceholder"}
+                  />
+                )}
+              </Field>
+            )}
+            {token && (
+              <Text subHeadline color="$uiNeutralPlaceholder">
+                {`/ ${Number(formatUnits(balance, token.decimals)).toFixed(4)} ${token.symbol}`}
+              </Text>
+            )}
+          </XStack>
+          <XStack paddingHorizontal="$s3">
+            {isLoading && !isActive ? (
+              <View flex={1}>
+                <Skeleton height={28} width={100} />
+              </View>
+            ) : (
+              <Text callout color="$uiNeutralPlaceholder">
+                {`${valueUSD > 0 ? "≈" : ""}${valueUSD.toLocaleString(undefined, {
+                  style: "currency",
+                  currency: "USD",
+                  currencyDisplay: "narrowSymbol",
+                })}`}
+              </Text>
+            )}
+          </XStack>
+        </YStack>
+      </XStack>
+    </YStack>
+  );
+}
+
+const AmountInput = styled(Input, {
+  flex: 1,
+  padding: 0,
+  unstyled: true,
+  fontSize: 28,
+  letterSpacing: -0.2,
+  textAlign: "left",
+  inputMode: "decimal",
+  paddingVertical: "$s2",
+  paddingHorizontal: "$s3",
+  placeholder: "0.00",
+  cursor: "pointer",
+  numberOfLines: 1,
+});
+
+const TokenImageContainer = styled(View, {
+  cursor: "pointer",
+  hitSlop: 20,
+  position: "relative",
+  width: 40,
+  height: 40,
+});
+
+const ChainLogoWrapper = styled(View, {
+  position: "absolute",
+  bottom: 0,
+  right: 0,
+  width: 20,
+  height: 20,
+  borderRadius: 10,
+  borderWidth: 2,
+  borderColor: "white",
+  overflow: "hidden",
+});
