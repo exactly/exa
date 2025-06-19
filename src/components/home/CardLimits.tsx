4797e3d95377cbf5e7ad3ddd2e48d9bcf70ee113
guillermo dieguez <guillermodieguez@outlook.com>
2025-06-19 18:44:43 -0300

    ✨ app: implement new home design

--- src/components/home/CardLimits.tsx
+++ src/components/home/CardLimits.tsx
@@ -1,12 +1,11 @@
 import { marketUSDCAddress, previewerAddress } from "@exactly/common/generated/chain";
 import { borrowLimit, withdrawLimit } from "@exactly/lib";
-import { Info } from "@tamagui/lucide-icons";
+import { ChevronRight, Info } from "@tamagui/lucide-icons";
 import { useQuery } from "@tanstack/react-query";
 import { router } from "expo-router";
 import React from "react";
 import { useTranslation } from "react-i18next";
-import { Pressable } from "react-native";
-import { View, XStack, YStack } from "tamagui";
+import { XStack, YStack } from "tamagui";
 import { zeroAddress } from "viem";
 import { useAccount } from "wagmi";
 
@@ -14,28 +13,44 @@ import { useReadPreviewerExactly } from "../../generated/contracts";
 import assetLogos from "../../utils/assetLogos";
 import { getCard } from "../../utils/server";
 import AssetLogo from "../shared/AssetLogo";
-import ProcessingBalance from "../shared/ProcessingBalance";
 import Text from "../shared/Text";
 
-export default function CardLimits({ onInfoPress }: { onInfoPress: () => void }) {
+export default function CardLimits({ onPress }: { onPress: () => void }) {
   const { t } = useTranslation();
-  const { data: card } = useQuery({ queryKey: ["card", "details"], queryFn: getCard });
-  const isCredit = card ? card.mode > 0 : false;
   const { address } = useAccount();
+  const { data: card } = useQuery({ queryKey: ["card", "details"], queryFn: getCard });
   const { data: markets } = useReadPreviewerExactly({ address: previewerAddress, args: [address ?? zeroAddress] });
+  const isCredit = card ? card.mode > 0 : false;
   return (
-    <YStack justifyContent="center" backgroundColor="$backgroundSoft" gap="$s4">
-      <View display="flex" flexDirection="row" justifyContent="center" alignItems="center" gap="$s2">
-        <Text emphasized subHeadline color="$uiNeutralSecondary" textAlign="center">
-          Spending limit
-        </Text>
-        <Pressable hitSlop={15} onPress={onInfoPress}>
-          <Info size={16} color="$uiBrandSecondary" />
-        </Pressable>
-      </View>
-      <View
-        alignSelf="center"
-        justifyContent="center"
+    <YStack justifyContent="space-between" height="100%">
+      <YStack justifyContent="center" gap="$s2" alignItems="flex-start">
+        <XStack justifyContent="center" alignItems="center" gap="$s3">
+          {isCredit ? null : <AssetLogo width={24} height={24} uri={assetLogos.USDC} />}
+          <Text
+            sensitive
+            textAlign="center"
+            fontFamily="$mono"
+            fontSize={24}
+            overflow="hidden"
+            maxFontSizeMultiplier={1}
+            color="white"
+          >
+            {(markets
+              ? Number(isCredit ? borrowLimit(markets, marketUSDCAddress) : withdrawLimit(markets, marketUSDCAddress)) /
+                1e6
+              : 0
+            ).toLocaleString(undefined, { style: "currency", currency: "USD", currencyDisplay: "narrowSymbol" })}
+          </Text>
+        </XStack>
+        <XStack justifyContent="center" alignItems="center" gap="$s2" hitSlop={15} onPress={onPress} cursor="pointer">
+          <Text emphasized footnote secondary color="white">
+            SPENDING LIMIT
+          </Text>
+          <Info size={12} color="white" />
+        </XStack>
+      </YStack>
+      <XStack
+        alignSelf="flex-start"
         alignItems="center"
         backgroundColor={isCredit ? "$cardCreditInteractive" : "$cardDebitInteractive"}
         borderRadius="$r2"
@@ -48,33 +63,15 @@ export default function CardLimits({ onInfoPress }: { onInfoPress: () => void })
       >
         <Text
           emphasized
+          footnote
           textTransform="uppercase"
           color={isCredit ? "$cardCreditText" : "$cardDebitText"}
           maxFontSizeMultiplier={1}
         >
-          {isCredit ? t("Pay in {{count}} installments enabled", { count: card?.mode }) : t("Pay Now enabled")}
+          {isCredit ? t("{{count}} installments", { count: card?.mode }) : t("Pay Now")}
         </Text>
-      </View>
-      <YStack gap="$s3" alignItems="center">
-        <XStack justifyContent="center" alignItems="center" gap="$s3">
-          {isCredit ? null : <AssetLogo width={32} height={32} uri={assetLogos.USDC} />}
-          <Text
-            sensitive
-            textAlign="center"
-            fontFamily="$mono"
-            fontSize={40}
-            overflow="hidden"
-            maxFontSizeMultiplier={1}
-          >
-            {(markets
-              ? Number(isCredit ? borrowLimit(markets, marketUSDCAddress) : withdrawLimit(markets, marketUSDCAddress)) /
-                1e6
-              : 0
-            ).toLocaleString(undefined, { style: "currency", currency: "USD", currencyDisplay: "narrowSymbol" })}
-          </Text>
-        </XStack>
-        <ProcessingBalance />
-      </YStack>
+        <ChevronRight size={16} color={isCredit ? "$cardCreditText" : "$cardDebitText"} />
+      </XStack>
     </YStack>
   );
 }
