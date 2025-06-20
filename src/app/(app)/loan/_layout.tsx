54066eec102cce83b8ba256b5779329670f139d9
guillermo dieguez <guillermodieguez@outlook.com>
2025-06-20 12:44:13 -0300

    🐛 app: remove modal presentation on loan flow

--- src/app/(app)/loan/_layout.tsx
+++ src/app/(app)/loan/_layout.tsx
@@ -5,5 +5,5 @@ import useBackgroundColor from "../../../utils/useBackgroundColor";
 
 export default function LoansLayout() {
   useBackgroundColor();
-  return <Stack initialRouteName="index" screenOptions={{ headerShown: false, presentation: "modal" }} />;
+  return <Stack initialRouteName="index" screenOptions={{ headerShown: false }} />;
 }
