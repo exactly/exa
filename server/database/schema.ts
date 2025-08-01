727e6deda686fc1c8557c03e4a25487c8b5e2699
nfmelendez <nfmelendez@gmail.com>
2025-08-01 13:12:09 -0300

    🗃️ server: add source database table

--- server/database/schema.ts
+++ server/database/schema.ts
@@ -57,7 +57,15 @@ export const transactions = pgTable("transactions", {
   payload: jsonb("payload").notNull(),
 });
 
-export const credentialsRelations = relations(credentials, ({ many }) => ({ cards: many(cards) }));
+export const sources = pgTable("sources", {
+  id: text("id").primaryKey(),
+  config: jsonb("config").notNull(),
+});
+
+export const credentialsRelations = relations(credentials, ({ many, one }) => ({
+  cards: many(cards),
+  source: one(sources, { fields: [credentials.source], references: [sources.id] }),
+}));
 
 export const cardsRelations = relations(cards, ({ many, one }) => ({
   credential: one(credentials, { fields: [cards.credentialId], references: [credentials.id] }),
