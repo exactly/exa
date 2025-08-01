662163095c9c784bc7c84cb95f76ec4ff317bea2
mainqueg <mainquegreen@gmail.com>
2025-08-01 18:04:18 -0300

    📝 server: document card api

--- server/utils/panda.ts
+++ server/utils/panda.ts
@@ -185,11 +185,13 @@ const CreateCardRequest = object({
   configuration: object({ productId: string(), virtualCardArt: string() }),
 });
 
+export const CardStatus = picklist(["active", "canceled", "locked", "notActivated"]);
+
 const CardResponse = object({
   id: string(),
   userId: string(),
   type: literal("virtual"),
-  status: picklist(["active", "canceled", "locked", "notActivated"]),
+  status: CardStatus,
   limit: object({
     amount: number(),
     frequency: picklist([
