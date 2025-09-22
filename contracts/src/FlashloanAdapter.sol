a357b98b628dfcf2cf3de75dae6969b80165afb5
itofarina <farinalvaro@gmail.com>
2025-09-22 18:08:00 -0300

    🚧

--- contracts/src/FlashloanAdapter.sol
+++ contracts/src/FlashloanAdapter.sol
@@ -3,13 +3,17 @@ pragma solidity ^0.8.24;
 
 import { AccessControl } from "openzeppelin-contracts/contracts/access/AccessControl.sol";
 import { IERC20 } from "openzeppelin-contracts/contracts/interfaces/IERC20.sol";
+import { IERC4626 } from "openzeppelin-contracts/contracts/interfaces/IERC4626.sol";
 
 import { IFlashLoaner } from "./IExaAccount.sol";
+
+import { console } from "forge-std/console.sol";
 
 contract FlashLoanAdapter is AccessControl, IFlashLoaner {
   IBalancerVaultV3 public immutable VAULT;
 
-  mapping(IERC20 asset => IAToken aToken) public aTokens;
+  mapping(IERC20 asset => bool isWAToken) public isWAToken;
+  mapping(IERC20 asset => IWAToken wAToken) public wATokens;
 
   constructor(IBalancerVaultV3 _vault, address owner) {
     _grantRole(DEFAULT_ADMIN_ROLE, owner);
@@ -21,11 +25,14 @@ contract FlashLoanAdapter is AccessControl, IFlashLoaner {
     override
   {
     for (uint256 i; i < tokens.length; ++i) {
-      if (tokens[i].balanceOf(address(VAULT)) < amounts[i]) {
-        if (aTokens[tokens[i]].balanceOf(address(VAULT)) < amounts[i]) {
+      uint256 amount = amounts[i];
+      if (tokens[i].balanceOf(address(VAULT)) < amount) {
+        IWAToken wAToken = wATokens[tokens[i]];
+        if (wAToken == IWAToken(address(0)) || wAToken.convertToAssets(wAToken.balanceOf(address(VAULT))) < amount) {
           revert InsufficientLiquidity();
         }
-        tokens[i] = IERC20(aTokens[tokens[i]]);
+        tokens[i] = IERC20(wAToken);
+        amounts[i] = wAToken.convertToShares(amount);
       }
     }
 
@@ -38,23 +45,54 @@ contract FlashLoanAdapter is AccessControl, IFlashLoaner {
     (address recipient, IERC20[] memory tokens, uint256[] memory amounts, bytes memory userData) =
       abi.decode(payload, (address, IERC20[], uint256[], bytes));
 
+    IERC20[] memory consumerTokens = new IERC20[](tokens.length);
+    uint256[] memory consumerAmounts = new uint256[](tokens.length);
+
     for (uint256 i; i < tokens.length; ++i) {
-      // TODO if it's an aToken, sendTo address(this), withdraw and send to recipient
-      VAULT.sendTo(tokens[i], recipient, amounts[i]);
+      uint256 amount = amounts[i];
+      IERC20 token = tokens[i];
+      if (isWAToken[token]) {
+        IWAToken wAToken = IWAToken(address(token));
+        VAULT.sendTo(token, address(this), amount);
+        wAToken.redeem(amount, recipient, address(this));
+        consumerTokens[i] = IERC20(wAToken.asset());
+        consumerAmounts[i] = wAToken.convertToAssets(amount);
+      } else {
+        consumerTokens[i] = token;
+        consumerAmounts[i] = amount;
+        VAULT.sendTo(token, recipient, amount);
+      }
     }
 
-    IFlashLoanRecipientV2(recipient).receiveFlashLoan(tokens, amounts, new uint256[](tokens.length), userData);
+    IFlashLoanRecipientV2(recipient).receiveFlashLoan(
+      consumerTokens, consumerAmounts, new uint256[](tokens.length), userData
+    );
+    console.log("received flash loan");
 
     for (uint256 i; i < tokens.length; ++i) {
-      // TODO if it's an aToken, supply to the pool and transfer to the vault
-      tokens[i].transfer(address(VAULT), amounts[i]);
-      VAULT.settle(tokens[i], amounts[i]);
+      IERC20 token = tokens[i];
+      uint256 amount = amounts[i];
+
+      if (isWAToken[token]) {
+        IWAToken wAToken = IWAToken(address(token));
+        console.log("depositing");
+        console.log("token", address(token));
+        console.log("amount", amount);
+        IERC20(wAToken.asset()).approve(address(wAToken), type(uint256).max);
+        // IERC20(wAToken.asset()).approve(address(VAULT), wAToken.convertToAssets(amount));
+        wAToken.deposit(wAToken.convertToAssets(amount), address(VAULT));
+        VAULT.settle(token, amount);
+      } else {
+        token.transfer(address(VAULT), amount);
+        VAULT.settle(token, amount);
+      }
     }
   }
 
-  function setAToken(IERC20 asset, IAToken token) external onlyRole(DEFAULT_ADMIN_ROLE) {
-    aTokens[asset] = token;
-    emit ATokenSet(asset, token, msg.sender);
+  function setWAToken(IERC20 asset, IWAToken token) external onlyRole(DEFAULT_ADMIN_ROLE) {
+    wATokens[asset] = token;
+    isWAToken[token] = true;
+    emit WATokenSet(asset, token, msg.sender);
   }
 }
 
@@ -73,6 +111,10 @@ interface IFlashLoanRecipientV2 {
   ) external;
 }
 
+interface IWAToken is IERC4626 {
+  function aToken() external view returns (IAToken);
+}
+
 interface IAToken is IERC20 {
   function burn(address from, address receiverOfUnderlying, uint256 amount, uint256 index) external;
   function mint(address caller, address onBehalfOf, uint256 amount, uint256 index) external returns (bool);
@@ -83,4 +125,4 @@ interface IAToken is IERC20 {
 error InsufficientLiquidity();
 error UnauthorizedVault();
 
-event ATokenSet(IERC20 indexed asset, IAToken indexed aToken, address indexed account);
+event WATokenSet(IERC20 indexed asset, IWAToken indexed wAToken, address indexed account);
--- contracts/test/FlashloanAdapter.t.sol
+++ contracts/test/FlashloanAdapter.t.sol
@@ -7,8 +7,10 @@ import { IERC20 } from "openzeppelin-contracts/contracts/interfaces/IERC20.sol";
 import { ForkTest } from "./Fork.t.sol";
 
 import {
-  ATokenSet, FlashLoanAdapter, IAToken, IBalancerVaultV3, IFlashLoanRecipientV2
+  FlashLoanAdapter, IBalancerVaultV3, IFlashLoanRecipientV2, IWAToken, WATokenSet
 } from "../src/FlashloanAdapter.sol";
+
+import { console } from "forge-std/console.sol";
 
 contract FlashloanAdapterTest is ForkTest {
   FlashLoanAdapter internal adapter;
@@ -25,23 +27,23 @@ contract FlashloanAdapterTest is ForkTest {
 
   // solhint-disable func-name-mixedcase
 
-  function test_setAToken_sets_whenAdmin() external {
+  function test_setWAToken_sets_whenAdmin() external {
     IERC20 asset = IERC20(address(0x1));
-    IAToken aToken = IAToken(address(0x2));
+    IWAToken wAToken = IWAToken(address(0x2));
 
-    adapter.setAToken(asset, aToken);
-    assertEq(address(adapter.aTokens(asset)), address(aToken), "aToken not set");
+    adapter.setWAToken(asset, wAToken);
+    assertEq(address(adapter.wATokens(asset)), address(wAToken), "wAToken not set");
   }
 
-  function test_setAToken_emitsATokenSet() external {
+  function test_setWAToken_emitsWATokenSet() external {
     IERC20 asset = IERC20(address(0x1));
-    IAToken aToken = IAToken(address(0x2));
+    IWAToken wAToken = IWAToken(address(0x2));
     vm.expectEmit(true, true, true, true, address(adapter));
-    emit ATokenSet(asset, aToken, address(this));
-    adapter.setAToken(asset, aToken);
+    emit WATokenSet(asset, wAToken, address(this));
+    adapter.setWAToken(asset, wAToken);
   }
 
-  function test_setAToken_reverts_whenNotAdmin() external {
+  function test_setWAToken_reverts_whenNotAdmin() external {
     address nonAdmin = address(0x1);
     vm.startPrank(nonAdmin);
     vm.expectRevert(
@@ -49,13 +51,21 @@ contract FlashloanAdapterTest is ForkTest {
         IAccessControl.AccessControlUnauthorizedAccount.selector, nonAdmin, adapter.DEFAULT_ADMIN_ROLE()
       )
     );
-    adapter.setAToken(IERC20(address(0x1)), IAToken(address(0x2)));
-    assertEq(address(adapter.aTokens(IERC20(address(0x1)))), address(0), "aToken set");
+    adapter.setWAToken(IERC20(address(0x1)), IWAToken(address(0x2)));
+    assertEq(address(adapter.wATokens(IERC20(address(0x1)))), address(0), "wAToken set");
   }
 
   function test_consumeAdapter() external {
     IERC20 rETH = IERC20(0x9Bcef72be871e61ED4fBbc7630889beE758eb81D);
     FlashLoanConsumer consumer = new FlashLoanConsumer(adapter, rETH);
+    consumer.callFlashLoan();
+  }
+
+  function test_consumeAdapter_withAToken() external {
+    IERC20 usdc = IERC20(protocol("USDC"));
+    IWAToken waOptUSDCn = IWAToken(address(0x41B334E9F2C0ED1f30fD7c351874a6071C53a78E));
+    adapter.setWAToken(usdc, waOptUSDCn);
+    FlashLoanConsumer consumer = new FlashLoanConsumer(adapter, usdc);
     consumer.callFlashLoan();
   }
 
@@ -75,7 +85,7 @@ contract FlashLoanConsumer is IFlashLoanRecipientV2 {
     IERC20[] memory tokens = new IERC20[](1);
     tokens[0] = token;
     uint256[] memory amounts = new uint256[](1);
-    amounts[0] = 1 ether;
+    amounts[0] = 1e6;
 
     adapter.flashLoan(address(this), tokens, amounts, "");
   }
@@ -83,6 +93,11 @@ contract FlashLoanConsumer is IFlashLoanRecipientV2 {
   function receiveFlashLoan(IERC20[] calldata tokens, uint256[] calldata amounts, uint256[] calldata, bytes calldata)
     external
   {
+    console.log("consumer.receiveFlashLoan");
+    console.log("tokens", address(tokens[0]));
+    console.log("amounts", amounts[0]);
+    console.log("balances", tokens[0].balanceOf(address(this)));
     tokens[0].transfer(address(adapter), amounts[0]);
+    console.log("consumer transferred");
   }
 }
