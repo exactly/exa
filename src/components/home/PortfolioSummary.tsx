503e0b3565a22e410a77f070b70b29446f25e2f1
guillermo dieguez <guillermodieguez@outlook.com>
2025-10-21 14:57:55 -0300

    ✨ app: implement weighted apr

--- src/components/home/PortfolioSummary.tsx
+++ src/components/home/PortfolioSummary.tsx
@@ -1,17 +1,30 @@
-import { ChevronRight } from "@tamagui/lucide-icons";
+import { ChevronRight, TrendingUp } from "@tamagui/lucide-icons";
 import { useQuery } from "@tanstack/react-query";
 import { useNavigation } from "expo-router";
 import React from "react";
 import { XStack, YStack } from "tamagui";
 
 import type { AppNavigationProperties } from "../../app/(main)/_layout";
+import assetLogos from "../../utils/assetLogos";
 import isProcessing from "../../utils/isProcessing";
+import reportError from "../../utils/reportError";
 import { getActivity } from "../../utils/server";
+import useIntercom from "../../utils/useIntercom";
+import AssetLogo from "../shared/AssetLogo";
 import Text from "../shared/Text";
 
-export default function PortfolioSummary({ usdBalance }: { usdBalance: bigint }) {
+export default function PortfolioSummary({
+  averageRate,
+  portfolio,
+}: {
+  averageRate: bigint;
+  portfolio: { usdBalance: bigint; depositMarkets: { market: string; symbol: string; usdValue: bigint }[] };
+}) {
+  const { usdBalance, depositMarkets } = portfolio;
   const navigation = useNavigation<AppNavigationProperties>();
+  const { presentArticle } = useIntercom();
   const { data: country } = useQuery({ queryKey: ["user", "country"] });
+
   const { data: processingBalance } = useQuery({
     queryKey: ["processing-balance"],
     queryFn: () => getActivity(),
@@ -77,7 +90,39 @@ export default function PortfolioSummary({ usdBalance }: { usdBalance: bigint })
           })}`}</Text>
           <ChevronRight size={16} color="$uiNeutralSecondary" />
         </XStack>
-      ) : null}
+      ) : (
+        <XStack
+          backgroundColor="$interactiveBaseSuccessSoftDefault"
+          padding="$s3"
+          borderRadius="$r_0"
+          gap="$s2"
+          alignItems="center"
+          cursor="pointer"
+          onPress={(event) => {
+            event.stopPropagation();
+            presentArticle("12633694").catch(reportError);
+          }}
+        >
+          <TrendingUp size={16} color="$interactiveOnBaseBrandSoft" />
+          <Text emphasized caption color="$interactiveOnBaseBrandSoft" textAlign="left">
+            {`${(Number(averageRate) / 1e18).toLocaleString(undefined, {
+              style: "percent",
+              minimumFractionDigits: 0,
+              maximumFractionDigits: 2,
+            })} APR`}
+          </Text>
+          <XStack alignItems="center">
+            {depositMarkets.map(({ market, symbol }, index, array) => {
+              const uri = assetLogos[symbol as keyof typeof assetLogos];
+              return (
+                <XStack key={market} marginRight={index < array.length - 1 ? -6 : 0} zIndex={array.length - index}>
+                  <AssetLogo uri={uri} width={16} height={16} />
+                </XStack>
+              );
+            })}
+          </XStack>
+        </XStack>
+      )}
     </YStack>
   );
 }
