0d528be1b8992be3804ae247f322ef475e3890f1
danilo neves cruz <cruzdanilo@gmail.com>
2025-10-21 16:31:19 -0300

    🔧 server: setup sentry for base networks

--- server/instrument.cjs
+++ server/instrument.cjs
@@ -5,7 +5,13 @@ const { nodeProfilingIntegration } = require("@sentry/profiling-node");
 init({
   dsn: process.env.SENTRY_DSN,
   release: require("./generated/release"),
-  environment: { "web.exactly.app": "production", "sandbox.exactly.app": "sandbox" }[domain] ?? domain,
+  environment:
+    {
+      "web.exactly.app": "production",
+      "sandbox.exactly.app": "sandbox",
+      "base.exactly.app": "base-sepolia",
+      "base-sepolia.exactly.app": "base-sepolia",
+    }[domain] ?? domain,
   tracesSampleRate: 1,
   profilesSampleRate: 1,
   attachStacktrace: true,
