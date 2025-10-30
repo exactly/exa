71e9c40411b64df4574467d6ba2ae357fdd08e73
danilo neves cruz <cruzdanilo@gmail.com>
2025-10-30 16:50:05 -0300

    🔧 contracts: setup forge lint

--- contracts/script/Base.s.sol
+++ contracts/script/Base.s.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.0;
 
+/// forge-lint: disable-next-line(unused-import)
 import { ForkTest, stdError, stdJson } from "../test/Fork.t.sol"; // solhint-disable-line no-unused-import
 
-// solhint-disable-next-line no-empty-blocks
-abstract contract BaseScript is ForkTest { }
+abstract contract BaseScript is ForkTest { } // solhint-disable-line no-empty-blocks
