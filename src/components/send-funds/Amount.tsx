bea832a73bcc4cf9ef28b87e9f0ae13578610b1d
guillermo dieguez <guillermodieguez@outlook.com>
2025-11-10 15:26:37 -0300

    ⚡️ app: implement asset logo caching

--- src/components/send-funds/Amount.tsx
+++ src/components/send-funds/Amount.tsx
@@ -432,7 +432,7 @@ export default function Amount() {
               <AssetLogo
                 {...(details.external
                   ? { external: true, source: { uri: external?.logoURI }, width: 16, height: 16, borderRadius: 20 }
-                  : { uri: assetLogos[details.symbol as keyof typeof assetLogos], width: 16, height: 16 })}
+                  : { source: { uri: assetLogos[details.symbol as keyof typeof assetLogos] }, width: 16, height: 16 })}
               />
             </XStack>
           </YStack>
