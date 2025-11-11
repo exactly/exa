ba6d8a568ed8615180b07fc4abfb5266e4a4ecec
nfmelendez <nfmelendez@gmail.com>
2025-11-11 14:08:03 -0300

    ✨ server: add merchant category code to webhooks

--- server/hooks/panda.ts
+++ server/hooks/panda.ts
@@ -1090,6 +1090,7 @@ const BaseWebhook = v.object({
     merchantCity: v.nullish(v.pipe(v.string(), v.trim())),
     merchantCountry: v.nullish(v.pipe(v.string(), v.trim())),
     merchantCategory: v.nullish(v.pipe(v.string(), v.trim())),
+    merchantCategoryCode: v.string(),
     merchantName: v.pipe(v.string(), v.trim()),
     authorizedAt: v.optional(v.pipe(v.string(), v.isoTimestamp())),
     authorizedAmount: v.nullish(v.number()),
