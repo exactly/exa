0f9e89b40e89f718f9132ff729c42299e74479ff
nfmelendez <nfmelendez@gmail.com>
2025-11-13 17:34:54 -0300

    ✨ server: allow account on firewall

--- server/hooks/persona.ts
+++ server/hooks/persona.ts
@@ -1,3 +1,4 @@
+import { firewallAbi, firewallAddress } from "@exactly/common/generated/chain";
 import { vValidator } from "@hono/valibot-validator";
 import { captureException, getActiveSpan, SEMANTIC_ATTRIBUTE_SENTRY_OP, setContext, setUser } from "@sentry/node";
 import { eq } from "drizzle-orm";
@@ -21,6 +22,7 @@ import {
 } from "valibot";
 
 import database, { credentials } from "../database/index";
+import keeper from "../utils/keeper";
 import { createUser } from "../utils/panda";
 import { headerValidator } from "../utils/persona";
 import validatorHook from "../utils/validatorHook";
@@ -162,6 +164,15 @@ export default new Hono().post(
     await database.update(credentials).set({ pandaId: id }).where(eq(credentials.id, referenceId));
 
     setContext("persona", { inquiryId: personaShareToken, pandaId: id });
+
+    if (firewallAddress) {
+      keeper
+        .exaSend(
+          { name: "exa.firewall", op: "exa.firewall", attributes: { account: credential.account, personaShareToken } },
+          { address: firewallAddress, functionName: "allow", args: [credential.account, true], abi: firewallAbi },
+        )
+        .catch((error: unknown) => captureException(error, { level: "error" }));
+    }
 
     return c.json({ id }, 200);
   },
