eef4adc4144c4f8c87defd11a00482529ecb54e8
franm <fran.marienhoff@gmail.com>
2025-11-22 15:16:01 -0300

    🚑 app: prevent wrong address display

--- src/utils/useAccount.ts
+++ src/utils/useAccount.ts
@@ -1,8 +1,9 @@
-import { getAddress } from "viem";
+import { getAddress, zeroAddress } from "viem";
 import { useAccount as useWagmiAccount, type UseAccountParameters } from "wagmi"; // eslint-disable-line no-restricted-imports
 
 export default function useAccount(parameters?: UseAccountParameters) {
   const account = useWagmiAccount(parameters);
+  if (account.address === zeroAddress) return { ...account, address: undefined, isConnected: false };
   return !parameters?.config && process.env.EXPO_PUBLIC_IMPERSONATE
     ? { ...account, address: account.address && getAddress(process.env.EXPO_PUBLIC_IMPERSONATE) }
     : account;
