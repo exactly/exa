2e201b491085daab6aab3bed121bebf966731f14
danilo neves cruz <cruzdanilo@gmail.com>
2025-12-07 18:00:42 -0300

    🔧 app: unify svgo config

--- svgr.config.mjs
+++ svgr.config.mjs
@@ -1,15 +1,22 @@
 import path from "node:path";
 
-/** @type {import('@svgr/core').Config} */
+import svgoConfig from "./svgo.config.mjs";
+
+/** @satisfies {import('@svgr/core').Config} */
 export default {
   svgoConfig: {
+    ...svgoConfig,
     plugins: [
-      { name: "preset-default" },
+      ...svgoConfig.plugins,
       {
         name: "prefixIds",
         params: {
-          prefix: (_, info) => {
-            if (info.path) return path.basename(info.path, ".svg");
+          /**
+           * @param {unknown} _
+           * @param {import('svgo').Config} config
+           */
+          prefix(_, config) {
+            if (config.path) return path.basename(config.path, ".svg");
             return "";
           },
         },
