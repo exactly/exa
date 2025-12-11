6eab23edc6a74a4eea6800db725c1b33287fe19a
danilo neves cruz <cruzdanilo@gmail.com>
2025-12-11 15:11:03 -0300

    🤡 server: close mock database after all tests

--- server/test/mocks/database.ts
+++ server/test/mocks/database.ts
@@ -1,14 +1,16 @@
 import { PGlite } from "@electric-sql/pglite";
 import type * as DrizzleKit from "drizzle-kit/api";
 import { drizzle, type PgliteDatabase } from "drizzle-orm/pglite";
-import { vi } from "vitest";
+import { afterAll, vi } from "vitest";
 
 import type * as database from "../../database";
 import * as schema from "../../database/schema";
 
-vi.mock("../../database", async (importOriginal) => {
+const pglite = new PGlite(undefined, { debug: 0 });
+
+vi.doMock("../../database", async (importOriginal) => {
   const { pushSchema } = require("drizzle-kit/api") as typeof DrizzleKit; // eslint-disable-line @typescript-eslint/no-require-imports, unicorn/prefer-module
-  const instance = drizzle(new PGlite(undefined, { debug: 0 }), { schema });
+  const instance = drizzle(pglite, { schema });
   const stdoutWrite = process.stdout.write; // eslint-disable-line @typescript-eslint/unbound-method
   process.stdout.write = () => true;
   const { apply } = await pushSchema(schema, instance as unknown as PgliteDatabase);
@@ -16,3 +18,5 @@ vi.mock("../../database", async (importOriginal) => {
   process.stdout.write = stdoutWrite;
   return { ...(await importOriginal<typeof database>()), default: instance };
 });
+
+afterAll(() => pglite.close());
