839f0e27835a0f268e8d7a795ba7549387ea6379
danilo neves cruz <cruzdanilo@gmail.com>
2025-12-11 14:41:02 -0300

    ⬆️ server: upgrade sentry

--- server/test/mocks/sentry.ts
+++ server/test/mocks/sentry.ts
@@ -1,34 +1,6 @@
-import { close, getSpanStatusFromHttpCode, startSpan } from "@sentry/node";
-import type * as hono from "hono";
-import { createMiddleware } from "hono/factory";
-import path from "node:path";
-import { afterAll, expect, vi } from "vitest";
+import "../../instrument.cjs";
 
-vi.mock("hono", async (importOriginal) => {
-  await import("../../instrument.cjs");
-  const { Hono, ...original } = await importOriginal<typeof hono>();
-  return {
-    ...original,
-    Hono: class extends Hono {
-      constructor(...arguments_: ConstructorParameters<typeof Hono>) {
-        super(...arguments_);
-        this.use(middleware);
-      }
-    },
-  };
-});
-
-const middleware = createMiddleware(async (c, next) => {
-  const { currentTestName, testPath } = expect.getState();
-  const testFile = path.relative(path.resolve(__dirname, ".."), testPath ?? ""); // eslint-disable-line unicorn/prefer-module
-  const name = `${c.req.method} /${testFile.replace(/\.test\.ts$/, "")}`;
-  return startSpan(
-    { name, op: "http.server", forceTransaction: true, attributes: { "test.name": currentTestName } },
-    async (span) => {
-      await next();
-      span.setStatus(getSpanStatusFromHttpCode(c.res.status));
-    },
-  );
-});
+import { close } from "@sentry/node";
+import { afterAll } from "vitest";
 
 afterAll(() => close());
