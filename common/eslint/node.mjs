9fb04fd1e709cf8e084c1811e333be0d23af3d7c
danilo neves cruz <cruzdanilo@gmail.com>
2025-12-13 10:38:10 -0300

    🚧 eslint: security

--- common/eslint/node.mjs
+++ common/eslint/node.mjs
@@ -1,8 +1,12 @@
 import vitest from "@vitest/eslint-plugin";
 import { defineConfig } from "eslint/config";
 import node from "eslint-plugin-n";
+import security from "eslint-plugin-security";
 
 export default defineConfig([
   node.configs["flat/recommended"],
+  // @ts-expect-error -- bad types
+  security.configs.recommended,
+  { rules: { "security/detect-object-injection": "off" } },
   { files: ["test/**"], plugins: { vitest }, rules: { ...vitest.configs.recommended.rules } },
 ]);
