268c149d56ee493e7d7f56a4a90004a2972d5c75
danilo neves cruz <cruzdanilo@gmail.com>
2025-12-15 10:39:44 -0300

    🚧 app: (re)fix ios build

--- babel.config.cjs
+++ babel.config.cjs
@@ -6,6 +6,24 @@ module.exports = function config(api) {
     plugins: [
       ...(process.env.EXPO_PUBLIC_ENV === "e2e" ? ["istanbul"] : []),
       [
+        "module-resolver",
+        {
+          alias: {
+            "@farcaster/quick-auth/decodeJwt": "@farcaster/quick-auth/dist/decodeJwt",
+            "@farcaster/quick-auth/light": "@farcaster/quick-auth/dist/lightClient",
+            "@wagmi/core/codegen": "@wagmi/core/dist/esm/exports/codegen",
+            "hono/client": "hono/dist/client",
+            "jose/jwt/decode": "jose/dist/browser/util/decode_jwt",
+            "^jose$": "jose/dist/browser/index.js",
+            "^zustand$": "zustand/index.js",
+            "zustand/middleware": "zustand/middleware.js",
+            "zustand/vanilla": "zustand/vanilla.js",
+            "zustand/shallow": "zustand/shallow.js",
+            "zustand/traditional": "zustand/traditional.js",
+          },
+        },
+      ],
+      [
         "@tamagui/babel-plugin",
         {
           config: "tamagui.config.ts",
