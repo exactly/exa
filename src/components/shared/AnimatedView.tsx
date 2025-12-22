2c28a06988c5808ad2a2fccbd09899a4f2e43720
guillermo dieguez <guillermodieguez@outlook.com>
2025-12-22 15:12:59 -0300

    🐛 app: fix forward ref in animated views

--- src/components/shared/AnimatedView.tsx
+++ src/components/shared/AnimatedView.tsx
@@ -1,4 +1,14 @@
+import React, { type ComponentPropsWithoutRef } from "react";
 import Animated from "react-native-reanimated";
 import { View } from "tamagui";
 
-export default Animated.createAnimatedComponent(View);
+const ViewWithForwardedReference = ({
+  forwardedRef,
+  ...properties
+}: ComponentPropsWithoutRef<typeof View> & {
+  forwardedRef?: React.Ref<React.ComponentRef<typeof View> | null>;
+}) => {
+  return <View ref={forwardedRef} {...properties} />;
+};
+ViewWithForwardedReference.displayName = "ViewWithForwardedRef";
+export default Animated.createAnimatedComponent(ViewWithForwardedReference);
