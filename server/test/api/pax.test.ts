08c6baeb29388ba2c8e1b0665e7cf26975fefbab
Miguel Diaz <github.com.hf06j@slmail.me>
2025-12-23 22:21:07 +0100

    🔧

--- server/test/api/pax.test.ts
+++ server/test/api/pax.test.ts
@@ -9,15 +9,13 @@ import { padHex, zeroAddress, zeroHash } from "viem";
 import { privateKeyToAddress } from "viem/accounts";
 import { afterEach, beforeAll, describe, expect, inject, it, vi } from "vitest";
 
-import app from "../../api/pax";
+import app, { type AppType } from "../../api/pax";
 import database, { credentials } from "../../database";
 import deriveAssociateId from "../../utils/deriveAssociateId";
 
+const appClient = testClient<AppType>(app);
 
-
-const appClient = testClient(app);
-
-describe("GET /associate", () => {
+describe("/pax GET", () => {
   const bob = privateKeyToAddress(padHex("0xb0b"));
   const account = deriveAddress(inject("ExaAccountFactory"), { x: padHex(bob), y: zeroHash });
 
@@ -38,18 +36,20 @@ describe("GET /associate", () => {
   });
 
   it("returns associate id", async () => {
-    const response = await appClient.associate.$get(
+    const response = await appClient.index.$get(
       {},
       { headers: { "test-credential-id": account, SessionID: "fakeSession" } },
     );
 
     const expectedId = deriveAssociateId(account);
+
     await expect(response.json()).resolves.toStrictEqual({ associateId: expectedId });
+
     expect(response.status).toBe(200);
   });
 
   it("returns 404 if credential not found", async () => {
-    const response = await appClient.associate.$get(
+    const response = await appClient.index.$get(
       {},
       { headers: { "test-credential-id": "non-existent", SessionID: "fakeSession" } },
     );
