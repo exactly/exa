b5f097901fee29d68f5bdbbdb067215332d02d8f
franm <fran.marienhoff@gmail.com>
2025-12-29 10:52:37 -0300

    🚑 app: fix cross-repay

---
+++ .changeset/smart-nails-clap.md
@@ -0,0 +1,5 @@
+---
+"@exactly/mobile": patch
+---
+
+🚑 fix cross-repay
--- src/utils/lifi.ts
+++ src/utils/lifi.ts
@@ -83,6 +83,7 @@ export async function getRoute(
     tool,
     estimate,
     toAmount: BigInt(estimate.toAmount),
+    fromAmount: BigInt(estimate.fromAmount),
   };
 }
 
