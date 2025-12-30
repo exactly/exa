3adf4798f6ba8617f043b1ade653999086badbe4
danilo neves cruz <cruzdanilo@gmail.com>
2025-12-30 07:54:41 -0300

    ✨ substreams: map exa accounts

--- substreams/src/lib.rs
+++ substreams/src/lib.rs
@@ -1,4 +1,34 @@
 #![expect(clippy::not_unsafe_ptr_arg_deref)]
 
+use serde::Deserialize;
+use substreams::{Hex, errors::Error};
+use substreams_ethereum::pb::eth::v2::Block;
+
+use crate::{contracts::factory::events::ExaAccountInitialized, proto::exa::Accounts};
+
 mod contracts;
 mod proto;
+
+#[derive(Debug, Deserialize)]
+struct Factories {
+  factories: Vec<String>,
+}
+
+#[substreams::handlers::map]
+pub fn map_exa_accounts(params: String, block: Block) -> Result<Accounts, Error> {
+  Ok(Accounts {
+    accounts: block
+      .events::<ExaAccountInitialized>(
+        &serde_qs::from_str::<Factories>(&params)?
+          .factories
+          .iter()
+          .map(Hex::decode)
+          .collect::<Result<Vec<_>, _>>()?
+          .iter()
+          .map(Vec::as_slice)
+          .collect::<Vec<_>>(),
+      )
+      .map(|(event, _)| event.account)
+      .collect(),
+  })
+}
