f1f6a8c4c3af3a7eda607628d442858514fd25e1
danilo neves cruz <cruzdanilo@gmail.com>
2025-12-30 11:16:14 -0300

    ✨ substreams: map exa plugins

--- substreams/src/lib.rs
+++ substreams/src/lib.rs
@@ -1,14 +1,18 @@
 #![expect(clippy::not_unsafe_ptr_arg_deref)]
 
 use serde::Deserialize;
+use std::collections::HashSet;
 use substreams::{
   Hex,
   errors::Error,
-  store::{StoreNew, StoreSet, StoreSetInt64},
+  store::{StoreGet, StoreGetInt64, StoreNew, StoreSet, StoreSetInt64},
 };
-use substreams_ethereum::pb::eth::v2::Block;
+use substreams_ethereum::{Event, pb::eth::v2::Block};
 
-use crate::{contracts::factory::events::ExaAccountInitialized, proto::exa::Accounts};
+use crate::{
+  contracts::{account::events::PluginInstalled, factory::events::ExaAccountInitialized},
+  proto::exa::{Accounts, Plugins, plugins::Plugin},
+};
 
 mod contracts;
 mod proto;
@@ -42,4 +46,24 @@ pub fn store_exa_accounts(new: Accounts, store: StoreSetInt64) {
   for account in new.accounts {
     store.set(0, Hex(&account).to_string(), &1);
   }
+}
+
+#[substreams::handlers::map]
+pub fn map_exa_plugins(block: Block, new: Accounts, accounts: StoreGetInt64) -> Result<Plugins, Error> {
+  let mut seen = HashSet::new();
+  Ok(Plugins {
+    plugins: block
+      .logs()
+      .filter_map(|log| {
+        let event = PluginInstalled::match_and_decode(log)?;
+        let address = log.address();
+        (if new.accounts.iter().any(|new_account| address == new_account.as_slice()) {
+          !seen.insert(address.to_vec())
+        } else {
+          accounts.get_last(Hex(&address).to_string())? == 1
+        })
+        .then(|| Plugin { address: event.plugin, account: address.to_vec(), ordinal: log.ordinal() })
+      })
+      .collect(),
+  })
 }
