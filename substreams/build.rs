2d4e0e72653e097ca0b3eadefb782940376a28a0
danilo neves cruz <cruzdanilo@gmail.com>
2025-12-30 11:16:14 -0300

    ✨ substreams: map exa plugins

--- substreams/build.rs
+++ substreams/build.rs
@@ -14,9 +14,10 @@ fn main() -> Result<(), Error> {
   println!("cargo::rerun-if-changed=proto");
   println!("cargo::rerun-if-changed=buf.gen.yaml");
   println!("cargo::rerun-if-changed=substreams.yaml");
-  println!("cargo::rerun-if-changed=node_modules/@exactly/plugin/src/ExaAccountFactory.sol");
+  println!("cargo::rerun-if-changed=node_modules/@exactly/plugin/src");
+  println!("cargo::rerun-if-changed=node_modules/@exactly/plugin/node_modules/modular-account");
 
-  let contracts = [("factory", "ExaAccountFactory")];
+  let contracts = [("account", "UpgradeableModularAccount"), ("factory", "ExaAccountFactory")];
   for (_, contract) in &contracts {
     println!("cargo::rerun-if-changed=node_modules/@exactly/plugin/out/{contract}.sol/{contract}.json");
   }
