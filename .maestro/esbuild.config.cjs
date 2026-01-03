45137950873933a0d02be4f39a450ee39bd4ff09
danilo neves cruz <cruzdanilo@gmail.com>
2026-01-03 12:36:39 -0300

    🔨 e2e: setup root development target

--- .maestro/esbuild.config.cjs
+++ .maestro/esbuild.config.cjs
@@ -1,3 +1,5 @@
+const path = require("node:path");
+
 /** @satisfies {import("esbuild").BuildOptions} */
 module.exports = {
   bundle: true,
@@ -6,4 +8,14 @@ module.exports = {
   external: ["isows"],
   mainFields: ["module", "main"],
   inject: ["src/polyfill.ts"],
+  absWorkingDir: __dirname,
+  plugins: [
+    {
+      name: "fix-tsconfig",
+      /** @param {import("esbuild").PluginBuild} build */
+      setup(build) {
+        build.initialOptions.tsconfig = path.join(__dirname, "tsconfig.json");
+      },
+    },
+  ],
 };
