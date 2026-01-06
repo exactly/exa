74e55525988d52ccb3b0ba06ad41166690029a2b
franm <fran.marienhoff@gmail.com>
2026-01-06 15:45:32 -0300

    🚸 app: improve browser opening across platforms

--- src/utils/openBrowser.ts
+++ src/utils/openBrowser.ts
@@ -2,5 +2,7 @@ import { sdk } from "@farcaster/miniapp-sdk";
 import { openBrowserAsync } from "expo-web-browser";
 
 export default async function openBrowser(url: string) {
-  await ((await sdk.isInMiniApp()) ? sdk.actions.openUrl(url) : openBrowserAsync(url));
+  await ((await sdk.isInMiniApp())
+    ? sdk.actions.openUrl(url)
+    : openBrowserAsync(url, { windowFeatures: { status: true, menubar: true } }));
 }
