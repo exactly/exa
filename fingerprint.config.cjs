da99d562ec2d7c28197002d111a32c5fac5ad109
danilo neves cruz <cruzdanilo@gmail.com>
2026-01-07 15:37:10 -0300

    🔧 app: setup environment variables in fingerprint

--- fingerprint.config.cjs
+++ fingerprint.config.cjs
@@ -1,5 +1,11 @@
+const { env } = require("node:process");
+
 /** @type {import('@expo/fingerprint').Config} */
 module.exports = {
   sourceSkips: ["ExpoConfigAll", "GitIgnore", "PackageJsonScriptsAll"],
   ignorePaths: ["package.json", "patches/*", "src/generated/versionCode.js"],
+  extraSources: [
+    { type: "contents", id: "APP_DOMAIN", contents: env.APP_DOMAIN ?? "" },
+    { type: "contents", id: "NODE_ENV", contents: env.NODE_ENV ?? "" },
+  ],
 };
