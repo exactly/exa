02b6de8d838b704b54a6b42bef5645a305918e59
danilo neves cruz <cruzdanilo@gmail.com>
2026-01-13 18:42:54 -0300

    🚧 e2e: android and web

--- src/utils/publicClient.ts
+++ src/utils/publicClient.ts
@@ -1,5 +1,9 @@
 import { alchemy, createAlchemyPublicRpcClient } from "@account-kit/infra";
 import alchemyAPIKey from "@exactly/common/alchemyAPIKey";
 import chain from "@exactly/common/generated/chain";
+import { http } from "viem";
 
-export default createAlchemyPublicRpcClient({ chain, transport: alchemy({ apiKey: alchemyAPIKey }) });
+export default createAlchemyPublicRpcClient({
+  chain,
+  transport: alchemyAPIKey ? alchemy({ apiKey: alchemyAPIKey }) : (http() as never),
+});
