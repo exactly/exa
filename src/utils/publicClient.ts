8f686fcaba1083fdf38df24d0499766a3e936dc2
danilo neves cruz <cruzdanilo@gmail.com>
2026-01-19 20:37:14 -0300

    🤡 app: use http transport for localhost

--- src/utils/publicClient.ts
+++ src/utils/publicClient.ts
@@ -1,6 +1,10 @@
 import { alchemy, createAlchemyPublicRpcClient } from "@account-kit/infra";
+import { http } from "viem";
 
 import alchemyAPIKey from "@exactly/common/alchemyAPIKey";
 import chain from "@exactly/common/generated/chain";
 
-export default createAlchemyPublicRpcClient({ chain, transport: alchemy({ apiKey: alchemyAPIKey }) });
+export default createAlchemyPublicRpcClient({
+  chain,
+  transport: alchemyAPIKey ? alchemy({ apiKey: alchemyAPIKey }) : (http() as never),
+});
