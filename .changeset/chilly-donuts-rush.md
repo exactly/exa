dfeb22945f8dd6959dc4d939be2558a3b2709749
Miguel Diaz <github.com.hf06j@slmail.me>
2026-01-20 15:43:49 +0100

    🐛 server: intercom token encoding

---
+++ .changeset/chilly-donuts-rush.md
@@ -0,0 +1,5 @@
+---
+"@exactly/server": patch
+---
+
+🐛 intercom token encoding
--- server/utils/intercom.ts
+++ server/utils/intercom.ts
@@ -1,13 +1,12 @@
 import { SignJWT } from "jose";
 
-const intercomSecret = process.env.INTERCOM_IDENTITY_KEY;
-
 if (!process.env.INTERCOM_IDENTITY_KEY) throw new Error("missing intercom key");
+const intercomSecret = process.env.INTERCOM_IDENTITY_KEY;
 
 export default async function getToken(userId: string, expires: Date | number) {
   return await new SignJWT({ sub: userId, user_id: userId })
     .setProtectedHeader({ alg: "HS256" })
     .setIssuedAt()
     .setExpirationTime(Math.floor((expires instanceof Date ? expires.getTime() : expires) / 1000))
-    .sign(new TextEncoder().encode(intercomSecret));
+    .sign(Buffer.from(intercomSecret, "base64"));
 }
