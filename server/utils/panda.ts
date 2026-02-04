571027188efaa3ce5c0ca11c68715e0261cb651e
nfmelendez <nfmelendez@gmail.com>
2026-02-04 09:55:13 -0300

    🔧 server: set card art for sandbox

--- server/utils/panda.ts
+++ server/utils/panda.ts
@@ -70,10 +70,13 @@ export async function createCard(userId: string, productId: typeof PLATINUM_PROD
       limit: { amount: 1_000_000, frequency: "per7DayPeriod" },
       configuration: {
         productId,
-        virtualCardArt: {
-          [PLATINUM_PRODUCT_ID]: "81e42f27affd4e328f19651d4f2b438e",
-          [SIGNATURE_PRODUCT_ID]: "398c4919514b4ec4927e6a9114a4c816",
-        }[productId],
+        virtualCardArt:
+          baseURL === "https://api-dev.rain.xyz/v1"
+            ? "0c515d7eb0a140fa8f938f8242b0780a"
+            : {
+                [PLATINUM_PRODUCT_ID]: "81e42f27affd4e328f19651d4f2b438e",
+                [SIGNATURE_PRODUCT_ID]: "398c4919514b4ec4927e6a9114a4c816",
+              }[productId],
       },
     }),
     "POST",
