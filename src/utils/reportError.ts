9c993705f771e124272483c5849f4dba7c5e68e8
danilo neves cruz <cruzdanilo@gmail.com>
2026-02-04 21:41:14 -0300

    🥅 app: handle error when reporting to sentry

--- src/utils/reportError.ts
+++ src/utils/reportError.ts
@@ -2,5 +2,9 @@ import { captureException } from "@sentry/react-native";
 
 export default function reportError(error: unknown, hint?: Parameters<typeof captureException>[1]) {
   console.error(error); // eslint-disable-line no-console
-  return captureException(error, hint);
+  try {
+    return captureException(error, hint);
+  } catch (sentryError) {
+    console.error(sentryError); // eslint-disable-line no-console
+  }
 }
