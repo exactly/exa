9270b651f6f7632e61578852c4622c0c4195824e
danilo neves cruz <cruzdanilo@gmail.com>
2026-02-09 15:19:52 -0300

    🔧 common: map base domains to panda certificates

--- common/pandaCertificate.ts
+++ common/pandaCertificate.ts
@@ -1,20 +1,26 @@
 import domain from "./domain";
 
-/* eslint-disable @typescript-eslint/prefer-nullish-coalescing -- ignore empty string */
-export default process.env.EXPO_PUBLIC_PANDA_PUBLIC_KEY ||
-  {
-    "web.exactly.app": `-----BEGIN PUBLIC KEY-----
+const production = `-----BEGIN PUBLIC KEY-----
 MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCeZ9uCoxi2XvOw1VmvVLo88TLk
 GE+OO1j3fa8HhYlJZZ7CCIAsaCorrU+ZpD5PUTnmME3DJk+JyY1BB3p8XI+C5uno
 QucrbxFbkM1lgR10ewz/LcuhleG0mrXL/bzUZbeJqI6v3c9bXvLPKlsordPanYBG
 FZkmBPxc8QEdRgH4awIDAQAB
------END PUBLIC KEY-----`,
-    "sandbox.exactly.app": `-----BEGIN PUBLIC KEY-----
+-----END PUBLIC KEY-----`;
+
+const sandbox = `-----BEGIN PUBLIC KEY-----
 MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCAP192809jZyaw62g/eTzJ3P9H
 +RmT88sXUYjQ0K8Bx+rJ83f22+9isKx+lo5UuV8tvOlKwvdDS/pVbzpG7D7NO45c
 0zkLOXwDHZkou8fuj8xhDO5Tq3GzcrabNLRLVz3dkx0znfzGOhnY4lkOMIdKxlQb
 LuVM/dGDC9UpulF+UwIDAQAB
------END PUBLIC KEY-----`,
+-----END PUBLIC KEY-----`;
+
+/* eslint-disable @typescript-eslint/prefer-nullish-coalescing -- ignore empty string */
+export default process.env.EXPO_PUBLIC_PANDA_PUBLIC_KEY ||
+  {
+    "web.exactly.app": production,
+    "base.exactly.app": production,
+    "base-sepolia.exactly.app": sandbox,
+    "sandbox.exactly.app": sandbox,
   }[domain] ||
   `-----BEGIN PUBLIC KEY-----
 MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCu2YOeObkaYiQmc49t2Cnk8syA
