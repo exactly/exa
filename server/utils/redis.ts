c65c11f407b5d5f18ae3a1d79c956c30ea66d20f
Miguel Diaz <github.com.hf06j@slmail.me>
2026-02-16 16:55:42 +0100

    🔧 server: split redis connections for bullmq compatibility

--- server/utils/redis.ts
+++ server/utils/redis.ts
@@ -2,4 +2,11 @@ import { Redis } from "ioredis";
 
 if (!process.env.REDIS_URL) throw new Error("missing redis url");
 
-export default new Redis(process.env.REDIS_URL);
+const redis = new Redis(process.env.REDIS_URL);
+export default redis;
+
+export const queue = new Redis(process.env.REDIS_URL, { maxRetriesPerRequest: null });
+
+export async function close() {
+  await Promise.all([queue.quit(), redis.quit()]);
+}
