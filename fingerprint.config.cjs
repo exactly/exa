b7df0f785cc905fb35fe7e44851eddd39b79e27c
danilo neves cruz <cruzdanilo@gmail.com>
2026-03-12 19:58:51 -0300

    🔧 app: include expo config in fingerprint

--- fingerprint.config.cjs
+++ fingerprint.config.cjs
@@ -2,7 +2,7 @@ const { env } = require("node:process");
 
 /** @type {import('@expo/fingerprint').Config} */
 module.exports = {
-  sourceSkips: ["ExpoConfigAll", "GitIgnore", "PackageJsonScriptsAll"],
+  sourceSkips: ["ExpoConfigVersions", "GitIgnore", "PackageJsonScriptsAll"],
   ignorePaths: ["package.json", "patches/*", "src/generated/versionCode.js"],
   extraSources: [
     { type: "contents", id: "APP_DOMAIN", contents: env.APP_DOMAIN ?? "" },
