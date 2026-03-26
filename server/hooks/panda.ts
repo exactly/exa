3f75d9468d466075e83f1b7ea3d5343b6d9a3462
nfmelendez <nfmelendez@gmail.com>
2026-03-26 15:55:56 -0300

    🔊 server: fix multiline webhook log

--- server/hooks/panda.ts
+++ server/hooks/panda.ts
@@ -1239,7 +1239,7 @@ async function publish(payload: v.InferOutput<typeof Payload>, receipt?: Transac
           },
         },
       );
-      debugWebhook({
+      debugWebhook("%j", {
         code: result.status,
         response: await result.text().then((text) => {
           try {
@@ -1253,9 +1253,9 @@ async function publish(payload: v.InferOutput<typeof Payload>, receipt?: Transac
     } catch (error) {
       if (error instanceof Error) {
         if (error.message === "WebhookFailed") {
-          debugWebhook(error.cause);
+          debugWebhook("%j", error.cause);
         } else {
-          debugWebhook({ error: error.message, payload: webhookPayload });
+          debugWebhook("%j", { error: error.message, payload: webhookPayload });
         }
       }
       throw error;
