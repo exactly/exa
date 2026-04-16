0a36648256ccc894090188fbea4875af1f54ca19
guillermo dieguez <dev@guillermodieguez.com>
2026-04-16 11:35:22 -0300

    🧱 app: add cross-chain account client support

--- src/utils/wagmi/exa.ts
+++ src/utils/wagmi/exa.ts
@@ -1,16 +1,19 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-import { createConfig, createStorage, custom } from "wagmi";
+import * as chains from "viem/chains";
+import { createConfig, createStorage, custom, http } from "wagmi";
 
 import chain from "@exactly/common/generated/chain";
 
 import alchemyConnector from "../alchemyConnector";
 import publicClient from "../publicClient";
 
+const others = Object.values(chains).filter((c) => c.id !== chain.id);
+
 export default createConfig({
-  chains: [chain],
+  chains: [chain, ...others],
   connectors: [alchemyConnector],
-  transports: { [chain.id]: custom(publicClient) },
+  transports: { ...Object.fromEntries(others.map((c) => [c.id, http()])), [chain.id]: custom(publicClient) },
   storage: createStorage({ key: "wagmi.exa", storage: AsyncStorage }),
   multiInjectedProviderDiscovery: false,
 });
