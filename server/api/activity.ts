916cf3c904c9b48d40f3cb2d5bb58334bfa913c0
guillermo dieguez <dev@guillermodieguez.com>
2026-05-18 17:24:00 -0300

    🚩 server: show declined transactions activity

--- server/api/activity.ts
+++ server/api/activity.ts
@@ -35,7 +35,6 @@ import {
   type InferOutput,
 } from "valibot";
 import { decodeFunctionData, zeroHash, type Log } from "viem";
-import { anvil } from "viem/chains";
 
 import fixedRate from "@exactly/common/fixedRate";
 import chain, {
@@ -426,7 +425,6 @@ export default new Hono().get(
       }),
     ]
       .filter(<T>(value: T | undefined): value is T => value !== undefined)
-      .filter((item) => chain.id === anvil.id || !("status" in item && item.status === "declined"))
       .toSorted((a, b) => b.timestamp.localeCompare(a.timestamp) || b.id.localeCompare(a.id));
 
     if (maturity !== undefined && pdf) {
