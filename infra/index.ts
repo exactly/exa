5f911a009504c862a56af51cb03bf34b48f0a0de
danilo neves cruz <cruzdanilo@gmail.com>
2026-07-22 17:12:13 +0200

    🧱 infra: setup poke queue worker

--- infra/index.ts
+++ infra/index.ts
@@ -12,6 +12,7 @@ const secretManager = new projects.Service("secretmanager", { service: "secretma
 
 const keyRing = new kms.KeyRing("signers", { location, name: `${stack}-signers` }, { dependsOn: cloudKms });
 const credit = new serviceaccount.Account("credit-worker", { accountId: `${stack}-credit` }, { dependsOn: iam });
+const poke = new serviceaccount.Account("poke-worker", { accountId: `${stack}-poke` }, { dependsOn: iam });
 const refund = new serviceaccount.Account("refund-worker", { accountId: `${stack}-refund` }, { dependsOn: iam });
 const subscribe = new serviceaccount.Account(
   "subscribe-worker",
@@ -33,6 +34,8 @@ const secrets = (<const S extends readonly string[]>(names: S) =>
   "credit-onesignal-api-key",
   "credit-postgres-url",
   "panda-api-url",
+  "poke-onesignal-api-key",
+  "poke-segment-write-key",
   "refund-panda-api-key",
   "redis-url",
   "sentry-dsn",
@@ -83,6 +86,61 @@ new cloudrunv2.WorkerPool(
         (secret) =>
           new secretmanager.SecretIamMember(`credit-${secret}-access`, {
             member: interpolate`serviceAccount:${credit.email}`,
+            role: "roles/secretmanager.secretAccessor",
+            secretId: secrets[secret].id,
+          }),
+      ),
+    ],
+  },
+);
+
+new cloudrunv2.WorkerPool(
+  "poke",
+  {
+    location,
+    name: `${stack}-poke`,
+    scaling: { manualInstanceCount: config.getNumber("pokeWorkers") ?? 1 },
+    template: {
+      serviceAccount: poke.email,
+      containers: [
+        {
+          image: serverImage,
+          resources: config.getObject("pokeResources"),
+          args: ["dist/workers/poke/worker.cjs"],
+          envs: [
+            { name: "APP_STACK", value: stack },
+            { name: "DEBUG", value: "exa:*" },
+            { name: "GCP_KMS_KEY_RING", value: keyRing.name },
+            { name: "GCP_KMS_KEY_VERSION", value: config.get("pokerVersion") ?? "1" },
+            { name: "GCP_KMS_LOCATION", value: location },
+            { name: "NODE_ENV", value: "production" },
+            { name: "SENTRY_DSN", valueSource: { secretKeyRef: { secret: `${stack}-sentry-dsn`, version: "latest" } } },
+          ],
+        },
+      ],
+    },
+  },
+  {
+    dependsOn: [
+      run,
+      new kms.CryptoKeyIAMMember("poke-signer", {
+        cryptoKeyId: new kms.CryptoKey(
+          "poker",
+          {
+            name: `${stack}-poker`,
+            purpose: "ASYMMETRIC_SIGN",
+            versionTemplate: { algorithm: "EC_SIGN_SECP256K1_SHA256", protectionLevel: "HSM" },
+            keyRing: keyRing.id,
+          },
+          { protect: true, retainOnDelete: true },
+        ).id,
+        member: interpolate`serviceAccount:${poke.email}`,
+        role: "roles/cloudkms.signerVerifier",
+      }),
+      ...(["poke-onesignal-api-key", "poke-segment-write-key", "redis-url", "sentry-dsn"] as const).map(
+        (secret) =>
+          new secretmanager.SecretIamMember(`poke-${secret}-access`, {
+            member: interpolate`serviceAccount:${poke.email}`,
             role: "roles/secretmanager.secretAccessor",
             secretId: secrets[secret].id,
           }),
