e55296c4703f570c58a2c654cd4a0b7b6cb2f11d
danilo neves cruz <cruzdanilo@gmail.com>
2026-07-23 16:49:40 +0200

    🔨 contracts: scope build to required artifacts

---
+++ contracts/test/support/Build.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.0;
+
+// solhint-disable no-unused-import, no-empty-blocks
+/// forge-lint: disable-start(unused-import)
+import { UpgradeableModularAccount } from "modular-account/src/account/UpgradeableModularAccount.sol";
+
+import { MockSwapper } from "../mocks/MockSwapper.sol";
+/// forge-lint: disable-end(unused-import)
+
+interface I { }
