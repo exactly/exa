05db719ba9fdb9ae1d23a1e68066b8d53d3fe3ef
danilo neves cruz <cruzdanilo@gmail.com>
2026-07-24 21:02:43 +0200

    ✨ server: introduce shared worker primitives

---
+++ server/workers/queue.ts
@@ -0,0 +1,47 @@
+import { spanToBaggageHeader, spanToTraceHeader, startSpan } from "@sentry/node";
+import { Queue, type BackoffOptions, type DefaultJobOptions, type KeepJobs } from "bullmq";
+
+import type { Redis } from "ioredis";
+
+export default function queue<Job extends Trace>(
+  name: string,
+  attempts: number,
+  bullmq: Redis,
+  options: Partial<Omit<DefaultJobOptions, "backoff" | "removeOnFail">> & {
+    backoff?: Partial<BackoffOptions>;
+    removeOnFail?: Partial<KeepJobs>;
+  } = {},
+) {
+  const instance = new Queue<Trace, void>(name, {
+    connection: bullmq,
+    defaultJobOptions: {
+      attempts,
+      removeOnComplete: true,
+      ...options,
+      backoff: { type: "exponential", delay: 1000, ...options.backoff },
+      removeOnFail: { count: 1000, age: 7 * 24 * 3600, ...options.removeOnFail },
+    },
+  });
+  return {
+    close: () => instance.close(),
+    async enqueue(data: Omit<Job, keyof Trace>, jobId: string, spanName: string = name) {
+      await startSpan(
+        { name: spanName, op: "queue.publish", attributes: { "messaging.destination.name": name } },
+        async (span) => {
+          const job = await instance.add(
+            name,
+            { ...data, sentryBaggage: spanToBaggageHeader(span), sentryTrace: spanToTraceHeader(span) },
+            { jobId },
+          );
+          span.setAttribute("messaging.message.id", job.id);
+          span.setAttribute("messaging.message.body.size", Buffer.byteLength(JSON.stringify(job.data)));
+        },
+      );
+    },
+  };
+}
+
+type Trace = {
+  sentryBaggage?: string;
+  sentryTrace?: string;
+};
